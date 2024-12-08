17a016c3c7d517f6ab76c0fdac35c736ee214775
Tariq5mo <tariqomer756@gmail.com>
2024-12-08 15:37:35 +0200

    Add task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,31 @@ in a log message.
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """
+
+        Args:
+            fields (List[str]): _description_
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''''''
+        record.msg = filter_datum(self.fields, RedactingFormatter.REDACTION,
+                                  record.msg, RedactingFormatter.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str],
