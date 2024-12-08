37f6d6142e1b61f0e8554450e8189ce4c686741a
Tariq5mo <tariqomer756@gmail.com>
2024-12-08 15:38:59 +0200

    Fix task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,15 +18,16 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]) -> None:
         """
+        Initialize the formatter with the fields to redact.
 
         Args:
-            fields (List[str]): _description_
+            fields (List[str]): A list of strings representing the fields to obfuscate.
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        ''''''
+        """Format the log record by obfuscating specified fields."""
         record.msg = filter_datum(self.fields, RedactingFormatter.REDACTION,
                                   record.msg, RedactingFormatter.SEPARATOR)
         return super().format(record)
