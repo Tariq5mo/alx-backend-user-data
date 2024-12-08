a860401ae32af640d8d77edc6394ade2f07f66e4
Tariq5mo <tariqomer756@gmail.com>
2024-12-08 15:43:46 +0200

    Fix task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,6 @@ This module provides a function to obfuscate specified fields
 in a log message.
 """
 import re
-from typing import List
 import logging
 
 
@@ -16,7 +15,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields) -> None:
         """
         Initialize the formatter with the fields to redact.
 
@@ -26,15 +25,15 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    def format(self, record: logging.LogRecord) -> str:
+    def format(self, record: logging.LogRecord):
         """Format the log record by obfuscating specified fields."""
         record.msg = filter_datum(self.fields, RedactingFormatter.REDACTION,
                                   record.msg, RedactingFormatter.SEPARATOR)
         return super().format(record)
 
 
-def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields,
+                 redaction, message, separator):
     """Obfuscate specified fields in the log message."""
     pattern = r'(' + '|'.join(fields) + r')=[^' + f'{separator}' + r']+'
     return re.sub(pattern, r'\1=' + redaction, message)
