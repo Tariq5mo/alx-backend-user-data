0dc07b7f4dceb12fce3624be5bd3a3e65df7e75e
Tariq5mo <tariqomer756@gmail.com>
2024-12-09 15:07:23 +0200

    Add 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,14 +8,17 @@ from typing import List
 import logging
 
 
+""" Task 2 """
+PII_FIELDS = ("email", "phone", "ssn", "password", "ip")
+
+
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class"""
 
     REDACTION: str = "***"
     FORMAT: str = (
         "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    )
+        )
     SEPARATOR: str = ";"
 
     def __init__(self, fields: List[str]):
@@ -25,13 +28,36 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """Format the log record by obfuscating specified fields."""
-        record.msg = filter_datum(self.fields, RedactingFormatter.REDACTION,
-                                  record.msg, RedactingFormatter.SEPARATOR)
+        record.msg = filter_datum(
+            self.fields,
+            RedactingFormatter.REDACTION,
+            record.msg,
+            RedactingFormatter.SEPARATOR,
+        )
         return super().format(record)
 
 
-def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """Obfuscate specified fields in the log message."""
-    pattern = r'(' + '|'.join(fields) + r')=[^' + f'{separator}' + r']+'
-    return re.sub(pattern, r'\1=' + redaction, message)
+    pattern = r"(" + "|".join(fields) + r")=[^" + f"{separator}" + r"]+"
+    return re.sub(pattern, r"\1=" + redaction, message)
+
+
+""" Task 2 """
+def get_logger() -> logging.Logger:
+    """
+
+    Returns:
+        logging.Logger: _description_
+    """
+    logger: logging.Logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+
+    logger.addHandler(handler)
+    return logger
