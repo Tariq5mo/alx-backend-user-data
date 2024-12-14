de284f45b206a3ffd0190e2453cc5803afe23166
Tariq5mo <tariqomer756@gmail.com>
2024-12-14 12:04:29 +0200

    Fix task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,17 +17,23 @@ class BasicAuth(Auth):
         self, authorization_header: str
     ) -> str:
         """
+        Extracts the Base64 part of
+        the Authorization header for Basic Authentication.
 
         Args:
-            authorization_header (str): _description_
+            authorization_header (str): The authorization header.
 
         Returns:
-            str: _description_
+            str: The Base64 part of the authorization header,
+            or None if invalid.
         """
-        auth_head = authorization_header
-        if isinstance(auth_head, str) and auth_head.startswith("Basic "):
-            return auth_head.split(" ")[1]
-        return None
+        if authorization_header is None or not isinstance(
+            authorization_header, str
+        ):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
 
     def decode_base64_authorization_header(
         self, base64_authorization_header: str
@@ -42,7 +48,9 @@ class BasicAuth(Auth):
         """
         try:
             if isinstance(base64_authorization_header, str):
-                decoded_str = base64.b64decode(base64_authorization_header)
+                decoded_str = base64.b64decode(
+                    base64_authorization_header
+                )
                 return decoded_str.decode("utf-8")
             return None
         except Exception:
@@ -71,7 +79,9 @@ class BasicAuth(Auth):
             self (_type_): _description_
         """
         if isinstance(user_email, str) and isinstance(user_pwd, str):
-            all_objs_email: List[User] = User.search({"email": user_email})
+            all_objs_email: List[User] = User.search(
+                {"email": user_email}
+            )
             if all_objs_email == []:
                 return None
             all_objs_pwd: List[User] = [
@@ -84,11 +94,14 @@ class BasicAuth(Auth):
             return all_objs_pwd[0]
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        """
+    def current_user(self, request=None) -> TypeVar("User"):
+        """ """
         header = self.authorization_header(request)
         extract_64 = self.extract_base64_authorization_header(header)
-        email_pwd_str = self.decode_base64_authorization_header(extract_64)
+        email_pwd_str = self.decode_base64_authorization_header(
+            extract_64
+        )
         email_pwd = self.extract_user_credentials(email_pwd_str)
-        return self.user_object_from_credentials(email_pwd[0], email_pwd[1])
+        return self.user_object_from_credentials(
+            email_pwd[0], email_pwd[1]
+        )
