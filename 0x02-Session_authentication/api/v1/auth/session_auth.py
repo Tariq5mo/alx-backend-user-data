c72c75888b2751764aedb6e792c2ad1a50052c30
Tariq5mo <tariqomer756@gmail.com>
2024-12-14 13:50:21 +0200

    Fix task 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+Session Authentication Module
 """
 from api.v1.auth.auth import Auth
 import uuid
@@ -7,21 +8,23 @@ import uuid
 
 class SessionAuth(Auth):
     """
-
-    Args:
-        Auth (_type_): _description_
+    SessionAuth class for managing session authentication.
+    Inherits from Auth.
     """
 
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """
+        Creates a Session ID for a user_id.
 
         Args:
-            user_id (str, optional): _description_. Defaults to None.
+            user_id (str, optional): The user ID for which
+                the session ID is created. Defaults to None.
 
         Returns:
-            str: _description_
+            str: The created session ID, or None if user_id is None
+                                                        or not a string.
         """
         if isinstance(user_id, str):
             session_id = str(uuid.uuid4())
@@ -31,12 +34,15 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
+        Returns a User ID based on a Session ID.
 
         Args:
-            session_id (str, optional): _description_. Defaults to None.
+            session_id (str, optional): The session ID to retrieve
+                                        the user ID for. Defaults to None.
 
         Returns:
-            str: _description_
+            str: The user ID associated with the session ID,
+                            or None if session_id is None or not a string.
         """
         if isinstance(session_id, str):
             return SessionAuth.user_id_by_session_id.get(session_id)
