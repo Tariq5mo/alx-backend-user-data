16f4ea7cb0e970a348af908af96be628fd41ee76
Tariq5mo <tariqomer756@gmail.com>
2024-12-22 14:47:04 +0200

    Add task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,9 @@
 import bcrypt
 from db import DB
 from bcrypt import gensalt, hashpw
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +15,29 @@ def _hash_password(password: str) -> bytes:
     """
     salt = gensalt()
     return hashpw(password.encode('utf-8'), salt)
+
+
+""" Task 5"""
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """This method should allow a user to register by providing an email
+        and password. It should then call the _hash_password method
+        to securely store the user's password in the database.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(
+                f"User {user.email} already exists"
+            )
+        except NoResultFound:
+            return self._db.add_user(
+                email, str(_hash_password(password))
+            )
